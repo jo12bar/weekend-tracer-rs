9f41847609c55a8ad298f2d5c86f1dedcdfb4f8d
Johann Barnard <johann.b@telus.net>
2020-04-27 00:29:04 -0700

    Switch to using indicatif for progress bars

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,3 +1,5 @@
+use indicatif::{ProgressBar, ProgressIterator, ProgressStyle};
+
 /// Render the scene. Outputs a vector of `u32`'s, one for each pixel:
 /// - The upper 8 bits is for the alpha channel.
 /// - The next 8 bits is for the red channel.
@@ -16,12 +18,15 @@ pub fn render_bgra(width: usize, height: usize) -> Vec<u32> {
 /// Render the scene. Outputs a vector of (r, g, b) integer triples, one for
 /// each pixel, which can range from 0 to 255.
 pub fn render(width: usize, height: usize) -> Vec<(u32, u32, u32)> {
-    let mut pb = pbr::ProgressBar::on(std::io::stderr(), (width * height) as u64);
-    pb.tick_format("▏▎▍▌▋▊▉██▉▊▋▌▍▎▏");
-    pb.show_message = true;
-    pb.message("Thread 1: ");
+    let pb_style = ProgressStyle::default_bar()
+        .template("{spinner} {msg} [{elapsed_precise}] [{bar:30.yellow/blue}] {pos}/{len}")
+        .progress_chars("=>-");
+
+    let pb = ProgressBar::new((width * height) as u64);
+    pb.set_style(pb_style);
 
     (0..(width * height))
+        .progress_with(pb)
         .map(|screen_pos| {
             let j = height - 1 - screen_pos / width;
             let i = screen_pos % width;
@@ -33,8 +38,6 @@ pub fn render(width: usize, height: usize) -> Vec<(u32, u32, u32)> {
             let ir = (255.999 * r) as u32;
             let ig = (255.999 * g) as u32;
             let ib = (255.999 * b) as u32;
-
-            pb.inc();
 
             (ir, ig, ib)
         })
