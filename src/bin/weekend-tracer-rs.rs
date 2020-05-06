86f623d41d4737ec6eefc0da5776994e5c83ae86
Johann Barnard <johann.b@telus.net>
2020-05-05 22:38:14 -0700

    Make some render options configurable on the command line

--- src/bin/weekend-tracer-rs.rs
+++ src/bin/weekend-tracer-rs.rs
@@ -5,86 +5,11 @@ use minifb::{Key, Window, WindowOptions};
 
 use weekend_tracer_rs::{bvh::BVH, camera::Camera, renderer, scenes, vec3, vec3::Vec3};
 
+// Some defaults
 const WIDTH: usize = 200;
 const HEIGHT: usize = 100;
 const SAMPLES_PER_PIXEL: usize = 100;
 const MAX_REFLECTION_DEPTH: usize = 50;
-
-const ASPECT_RATIO: f32 = (WIDTH as f32) / (HEIGHT as f32);
-
-/// Render to a simple cross-platform window using the `minifb` crate.
-#[cfg(feature = "gui-support")]
-fn gui_output(bvh: BVH, camera: Camera) {
-    let buffer: Vec<u32> = renderer::convert_to_argb(renderer::render(
-        WIDTH,
-        HEIGHT,
-        SAMPLES_PER_PIXEL,
-        MAX_REFLECTION_DEPTH,
-        bvh,
-        camera,
-    ))
-    .collect();
-
-    let mut window = Window::new(
-        "weekend-tracer-rs - ESC to exit",
-        WIDTH,
-        HEIGHT,
-        WindowOptions::default(),
-    )
-    .unwrap_or_else(|e| panic!("{}", e));
-
-    // Limit to max ~60 fps update rate
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
-
-    while window.is_open() && !window.is_key_down(Key::Escape) {
-        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
-    }
-}
-
-/// Render to an ASCII PPM `.ppm` file.
-fn ppm_output(filename: &str, bvh: BVH, camera: Camera) -> std::io::Result<()> {
-    let output = renderer::render(
-        WIDTH,
-        HEIGHT,
-        SAMPLES_PER_PIXEL,
-        MAX_REFLECTION_DEPTH,
-        bvh,
-        camera,
-    )
-    .into_iter()
-    .map(|(r, g, b)| format!("{} {} {}", r, g, b))
-    .fold(format!("P3\n{} {}\n255\n", WIDTH, HEIGHT), |s, pixel| {
-        s + &pixel + "\n"
-    });
-
-    std::fs::write(filename, output)
-}
-
-/// Render to some arbritrary image file type. Whatever the `image` crate
-/// supports.
-fn image_output(filename: &str, bvh: BVH, camera: Camera) {
-    let rendered = renderer::render(
-        WIDTH,
-        HEIGHT,
-        SAMPLES_PER_PIXEL,
-        MAX_REFLECTION_DEPTH,
-        bvh,
-        camera,
-    )
-    .into_iter()
-    .map(|(r, g, b)| vec![r as u8, g as u8, b as u8])
-    .flatten()
-    .collect::<Vec<_>>();
-
-    image::save_buffer(
-        filename,
-        &rendered[..],
-        WIDTH as u32,
-        HEIGHT as u32,
-        image::ColorType::Rgb8,
-    )
-    .unwrap();
-}
 
 fn main() {
     #[allow(unused_mut)]
@@ -101,6 +26,9 @@ fn main() {
             (@arg OUTPUT_FILE: required_unless[gui] "The file to be outputted to.")
             (@arg ppm: -p --ppm "Output to an ASCII PPM file (e.g. test.ppm, image.ppm, etc...).")
         )
+        (@arg dimensions: -d --dimensions <WIDTH> <HEIGHT> !required "Set the dimensions for the render. 200x100 by default.")
+        (@arg samples: -s --samples <SAMPLES_PER_PIXEL> !required "Sets the number of samples to be taken per pixel.")
+        (@arg reflections: -r --max_reflection_depth <DEPTH> !required "Sets the maximum reflection depth.")
     );
 
     #[cfg(feature = "gui-support")]
@@ -116,12 +44,48 @@ fn main() {
 
     let matches = app.get_matches();
 
+    let dimensions = if let Some(v) = matches.values_of("dimensions") {
+        v.map(str::parse::<usize>)
+            .map(|x| {
+                x.unwrap_or_else(|e| {
+                    panic!(
+                        "<WIDTH> or <HEIGHT> could not be parsed into a positive integer!\n{}",
+                        e
+                    )
+                })
+            })
+            .collect::<Vec<_>>()
+    } else {
+        vec![WIDTH, HEIGHT]
+    };
+
+    let width = dimensions[0];
+    let height = dimensions[1];
+
+    let samples_per_pixel = matches
+        .value_of("samples")
+        .unwrap_or(&SAMPLES_PER_PIXEL.to_string())
+        .parse::<usize>()
+        .unwrap_or_else(|e| {
+            panic!(
+                "Could not parse <SAMPLES_PER_PIXEL> into a positive integer!\n{}",
+                e
+            )
+        });
+
+    let max_reflection_depth = matches
+        .value_of("reflections")
+        .unwrap_or(&MAX_REFLECTION_DEPTH.to_string())
+        .parse::<usize>()
+        .unwrap_or_else(|e| panic!("Could not parse <DEPTH> into a positive integer!\n{}", e));
+
     let world = scenes::two_checkerboard_spheres();
     let bvh = BVH::new(world.objects, 0.0, 1.0);
 
     let lookfrom = vec3!(13.0, 2.0, 3.0);
     let lookat = vec3!(0.0, 0.0, 0.0);
     let vup = vec3!(0.0, 1.0);
+    let aspect_ratio = (width as f32) / (height as f32);
     let dist_to_focus = 10.0;
     let aperture = 0.0;
 
@@ -130,7 +94,7 @@ fn main() {
         lookat,
         vup,
         20.0,
-        ASPECT_RATIO,
+        aspect_ratio,
         aperture,
         dist_to_focus,
         0.0,
@@ -139,16 +103,137 @@ fn main() {
 
     if matches.is_present("gui") {
         #[cfg(feature = "gui-support")]
-        gui_output(bvh, camera);
+        gui_output(
+            bvh,
+            camera,
+            width,
+            height,
+            samples_per_pixel,
+            max_reflection_depth,
+        );
     } else {
         // Calling .unwrap() is safe here because we require that the OUTPUT_FILE
         // is present if --gui/-g is not present.
         let output_file = matches.value_of("OUTPUT_FILE").unwrap();
 
         if matches.is_present("ppm") {
-            ppm_output(output_file, bvh, camera).unwrap();
+            ppm_output(
+                output_file,
+                bvh,
+                camera,
+                width,
+                height,
+                samples_per_pixel,
+                max_reflection_depth,
+            )
+            .unwrap();
         } else {
-            image_output(output_file, bvh, camera);
+            image_output(
+                output_file,
+                bvh,
+                camera,
+                width,
+                height,
+                samples_per_pixel,
+                max_reflection_depth,
+            );
         }
     }
+}
+
+/// Render to a simple cross-platform window using the `minifb` crate.
+#[cfg(feature = "gui-support")]
+fn gui_output(
+    bvh: BVH,
+    camera: Camera,
+    width: usize,
+    height: usize,
+    samples_per_pixel: usize,
+    max_reflection_depth: usize,
+) {
+    let buffer: Vec<u32> = renderer::convert_to_argb(renderer::render(
+        width,
+        height,
+        samples_per_pixel,
+        max_reflection_depth,
+        bvh,
+        camera,
+    ))
+    .collect();
+
+    let mut window = Window::new(
+        "weekend-tracer-rs - ESC to exit",
+        width,
+        height,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| panic!("{}", e));
+
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        window.update_with_buffer(&buffer, width, height).unwrap();
+    }
+}
+
+/// Render to an ASCII PPM `.ppm` file.
+fn ppm_output(
+    filename: &str,
+    bvh: BVH,
+    camera: Camera,
+    width: usize,
+    height: usize,
+    samples_per_pixel: usize,
+    max_reflection_depth: usize,
+) -> std::io::Result<()> {
+    let output = renderer::render(
+        width,
+        height,
+        samples_per_pixel,
+        max_reflection_depth,
+        bvh,
+        camera,
+    )
+    .into_iter()
+    .map(|(r, g, b)| format!("{} {} {}", r, g, b))
+    .fold(format!("P3\n{} {}\n255\n", width, height), |s, pixel| {
+        s + &pixel + "\n"
+    });
+
+    std::fs::write(filename, output)
+}
+
+/// Render to some arbritrary image file type. Whatever the `image` crate
+/// supports.
+fn image_output(
+    filename: &str,
+    bvh: BVH,
+    camera: Camera,
+    width: usize,
+    height: usize,
+    samples_per_pixel: usize,
+    max_reflection_depth: usize,
+) {
+    let rendered = renderer::render(
+        width,
+        height,
+        samples_per_pixel,
+        max_reflection_depth,
+        bvh,
+        camera,
+    )
+    .into_iter()
+    .map(|(r, g, b)| vec![r as u8, g as u8, b as u8])
+    .flatten()
+    .collect::<Vec<_>>();
+
+    image::save_buffer(
+        filename,
+        &rendered[..],
+        width as u32,
+        height as u32,
+        image::ColorType::Rgb8,
+    )
+    .unwrap();
 }
