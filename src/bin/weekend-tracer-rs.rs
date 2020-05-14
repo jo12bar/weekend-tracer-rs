1c7d8c707557d71fdf4008c06cf2686343b96c4f
Johann Barnard <johann.b@telus.net>
2020-05-13 23:59:46 -0700

    Demonstrate stratified samples for calculating pi

--- src/bin/weekend-tracer-rs.rs
+++ src/bin/weekend-tracer-rs.rs
@@ -259,24 +259,38 @@ fn image_output(
 fn compute_pi() -> ! {
     use rand::prelude::*;
 
-    let mut runs = 0;
-    let mut inside_circle = 0;
     let mut rng = thread_rng();
+    let mut inside_circle: u64 = 0;
+    let mut inside_circle_stratified: u64 = 0;
+    let sqrt_n = 10_000;
 
-    loop {
-        runs += 1;
-        let x: f64 = rng.gen_range(-1.0, 1.0);
-        let y: f64 = rng.gen_range(-1.0, 1.0);
+    for i in 0..sqrt_n {
+        for j in 0..sqrt_n {
+            let mut x: f64 = rng.gen_range(-1.0, 1.0);
+            let mut y: f64 = rng.gen_range(-1.0, 1.0);
 
-        if x * x + y * y < 1.0 {
-            inside_circle += 1;
-        }
+            if x * x + y * y < 1.0 {
+                inside_circle += 1;
+            }
 
-        if runs % 100_000 == 0 {
-            println!(
-                "Estimate of Pi = {:.12}",
-                4.0 * (inside_circle as f64) / (runs as f64)
-            );
+            x = 2.0 * ((i as f64 + rng.gen::<f64>()) / sqrt_n as f64) - 1.0;
+            y = 2.0 * ((j as f64 + rng.gen::<f64>()) / sqrt_n as f64) - 1.0;
+
+            if x * x + y * y < 1.0 {
+                inside_circle_stratified += 1;
+            }
         }
     }
+
+    let n = (sqrt_n * sqrt_n) as f64;
+
+    println!(
+        "\
+        Regular estimate of Pi    = {:.12}\n\
+        Stratified estimate of Pi = {:.12}",
+        4.0 * (inside_circle as f64) / n,
+        4.0 * (inside_circle_stratified as f64) / n,
+    );
+
+    std::process::exit(0)
 }
